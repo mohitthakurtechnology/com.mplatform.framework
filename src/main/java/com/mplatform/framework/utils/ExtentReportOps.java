38e5ef84915e039c38fe509265c4dd7f2ad4eaf8
Nothing <mohit.thakur.technology@gmail.com>
2025-06-10 01:02:23 +0530

    fixed the file seperator problem

--- src/main/java/com/mplatform/framework/utils/ExtentReportOps.java
+++ src/main/java/com/mplatform/framework/utils/ExtentReportOps.java
@@ -1,5 +1,7 @@
 package com.mplatform.framework.utils;
 
+import java.io.File;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
@@ -9,7 +11,7 @@ public class ExtentReportOps
 	public static ExtentReports createExtentReporter(String projectName) 
     {	
     			
-    	String filePath = System.getProperty("user.dir") + "\\src\\test\\resources\\generatedReport\\" + projectName + ".html";
+    	String filePath = System.getProperty("user.dir") + File.separator + "src" + File.separator +"test" + File.separator + "resources" + File.separator + "generatedReport" + File.separator + projectName + ".html";
     	
     	System.out.println("filePath => " + filePath);
     	
